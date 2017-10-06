8c77c7713e21807c55309acca44cfb6ed3dd56d2
Vinicius Lelis <vinicius.ribeiro20@fatec.sp.gov.br>
2017-10-06 13:42:05 -0300

    Update FXMLDocumentController.java

--- fatec.SO.multiplasfilas/src/fatec/so/multiplasfilas/FXMLDocumentController.java
+++ fatec.SO.multiplasfilas/src/fatec/so/multiplasfilas/FXMLDocumentController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package fatec.so.multiplasfilas;
 
 import java.awt.Rectangle;
@@ -20,7 +16,7 @@ import fatec.so.multiplasfilas.Fila;
 
 /**
  *
- * @author Admin
+ * @author Vinicius Lelis
  */
 public class FXMLDocumentController implements Initializable {
     
